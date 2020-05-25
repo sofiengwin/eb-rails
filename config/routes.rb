7042b544c61b3e32cb98b82ec5d7d95b9ab553f0
Ogbara Godwin <godwin.onisofien@andela.com>
2020-05-25 23:55:27 +0100

    add books route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  resources :books
   get 'welcome_page/welcome'
-  root 'welcome_page#welcome'
+  root 'books#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
